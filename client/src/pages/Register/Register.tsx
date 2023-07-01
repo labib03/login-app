219c1fe3f862b06eb60a4284a9a73e05d7aa08b5
labib03 <ansorudinlabib1@gmail.com>
2023-07-01 22:39:39 +0700

    feat: slicing and refactor some code on register page

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -2,9 +2,12 @@ import { Container } from "../../components";
 import ProfileImage from "../../assets/profile.png";
 import { Link } from "react-router-dom";
 import { useFormik } from "formik";
-import { usernameValidate } from "../../helpers/validate";
+import { useState } from "preact/hooks";
 
 const Register = () => {
+  const [inputType, setInputType] = useState("password");
+  const [file, setFile] = useState("");
+
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -19,9 +22,29 @@ const Register = () => {
     },
   });
 
-  const isHasValue = formik?.values?.username?.length
-    ? formik?.values?.username?.length > 0
-    : false;
+  const isShown = (type: string) => {
+    const field = formik.getFieldProps(type);
+    return field.value ? field.value.length > 0 : false;
+  };
+
+  const changeTypeHandler = () => {
+    if (inputType === "password") {
+      setInputType("text");
+    } else {
+      setInputType("password");
+    }
+  };
+
+  const fieldResetHandler = (field: string) => {
+    const handler = formik.getFieldHelpers(field);
+    handler.setValue("");
+  };
+
+  // for handle file upload, cuz formik doesn't support file upload
+  const onUpload = async (e) => {
+    const base64 = "";
+    setFile(base64);
+  };
 
   return (
     <Container>
@@ -33,40 +56,96 @@ const Register = () => {
           </p>
         </div>
 
-        <div className="w-full text-center flex items-center justify-center mb-8">
-          <img
-            src={ProfileImage}
-            className="w-2/4 rounded-full border-2 border-white shadow-md"
-          />
+        <div className="mb-3">
+          <label
+            for="profile"
+            className="flex items-center justify-center hover:cursor-pointer"
+          >
+            <img
+              src={ProfileImage}
+              className="w-2/4 rounded-full border-2 border-white shadow-md"
+            />
+          </label>
+
+          <input type="file" id="profile" name="profile" className="hidden" />
+          <small className="text-xs italic">
+            *You can change profile photo
+          </small>
         </div>
 
-        <div className="relative w-full flex flex-col rounded-md overflow-hidden">
-          <input
-            {...formik.getFieldProps("email")}
-            type="text"
-            placeholder="Email"
-            className={`border w-full border-slate-100 pl-3 rounded-md  py-2 transition-all duration-200 focus:border-slate-300 placeholder:text-sm placeholder:text-center focus:placeholder:opacity-0  ${
-              isHasValue ? "pr-12 border-slate-300" : "pr-3"
-            }`}
-          />
-          {isHasValue && (
-            <button
-              className="h-full px-1.5 text-xs absolute right-0 top-0 bg-stone-50 border-t border-r border-b border-slate-300 rounded-e-md transition-all duration-200"
-              onClick={formik.handleReset}
-            >
-              clear
-            </button>
-          )}
+        <div className="flex flex-col gap-2">
+          <div className="relative w-full flex flex-col rounded-md overflow-hidden">
+            <input
+              {...formik.getFieldProps("email")}
+              type="text"
+              placeholder="Email"
+              className={`border w-full border-slate-100 pl-3 rounded-md  py-2 transition-all duration-200 focus:border-slate-300 placeholder:text-sm placeholder:text-center focus:placeholder:opacity-0  ${
+                isShown("email") ? "pr-12 border-slate-300" : "pr-3"
+              }`}
+            />
+            {isShown("email") && (
+              <button
+                className="h-full px-1.5 text-xs absolute right-0 top-0 bg-stone-50 border-t border-r border-b border-slate-300 rounded-e-md transition-all duration-200"
+                onClick={() => {
+                  fieldResetHandler("email");
+                  const check = formik.getFieldProps("email");
+                  console.log("check =>", check.value);
+                }}
+              >
+                clear
+              </button>
+            )}
+          </div>
+
+          <div className="relative w-full flex flex-col rounded-md overflow-hidden">
+            <input
+              {...formik.getFieldProps("username")}
+              type="text"
+              placeholder="Username"
+              className={`border w-full border-slate-100 pl-3 rounded-md  py-2 transition-all duration-200 focus:border-slate-300 placeholder:text-sm placeholder:text-center focus:placeholder:opacity-0  ${
+                isShown("username") ? "pr-12 border-slate-300" : "pr-3"
+              }`}
+            />
+            {isShown("username") && (
+              <button
+                className="h-full px-1.5 text-xs absolute right-0 top-0 bg-stone-50 border-t border-r border-b border-slate-300 rounded-e-md transition-all duration-200"
+                onClick={() => {
+                  fieldResetHandler("username");
+                }}
+              >
+                clear
+              </button>
+            )}
+          </div>
+
+          <div className="relative w-full flex flex-col rounded-md overflow-hidden">
+            <input
+              {...formik.getFieldProps("password")}
+              type={inputType}
+              placeholder="Password"
+              className={`border w-full border-slate-100 pl-3 rounded-md  py-2 transition-all duration-200 focus:border-slate-300 placeholder:text-sm placeholder:text-center focus:placeholder:opacity-0  ${
+                isShown("password") ? "pr-12 border-slate-300" : "pr-3"
+              }`}
+            />
+            {isShown("password") && (
+              <button
+                className="h-full px-1.5 text-xs absolute right-0 top-0 bg-stone-50 border-t border-r border-b border-slate-300 rounded-e-md transition-all duration-200"
+                onClick={changeTypeHandler}
+              >
+                {inputType === "password" ? "show" : "hide"}
+              </button>
+            )}
+          </div>
         </div>
 
         <button
-          className="bg-emerald-400 rounded-md py-2 text-sm mt-2 transition-all duration-150 hover:bg-emerald-500"
+          className="bg-emerald-400 rounded-md py-2 text-sm mt-3 transition-all duration-150 hover:bg-emerald-500"
           onClick={(e) => {
             e.preventDefault();
             formik.handleSubmit();
           }}
         >
-          Continue
+          Register
         </button>
 
         <div className="mb-4 mt-5">
