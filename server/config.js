61d5b44f93a722e64e070554ba36f3f59589abba
labib03 <ansorudinlabib1@gmail.com>
2023-07-05 17:48:58 +0700

    feat: connect to mongodb atlas

---
+++ server/config.js
@@ -0,0 +1,4 @@
+export default {
+  MONGO_URL:
+    "mongodb://raven:admin123@ac-ocslq5a-shard-00-00.w4cl2ub.mongodb.net:27017,ac-ocslq5a-shard-00-01.w4cl2ub.mongodb.net:27017,ac-ocslq5a-shard-00-02.w4cl2ub.mongodb.net:27017/?ssl=true&replicaSet=atlas-102c5n-shard-0&authSource=admin&retryWrites=true&w=majority",
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import ENV from "./config.js";
+import connect from "./databases/connection.js";
 import { router } from "./router/route.js";
 
 const app = express();
@@ -11,7 +13,8 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.disable("x-powered-by"); // less hackers know about our stack
 
-const port = 8080;
+const PORT = 8080;
+const MONGO_URL = ENV.MONGO_URL;
 
 // HTTPS Request
 app.get("/", (req, res) => {
@@ -24,20 +27,34 @@ app.get("/", (req, res) => {
 // Route
 app.use("/api", router);
 
-// connect()
+// mongoose
+//   .connect(MONGO_URL)
 //   .then(() => {
-//     try {
-//       app.listen(port, () =>
-//         console.log(`The server running on: http://localhost:${port}`)
+//     // listen app
+//     app.listen(PORT, () => {
+//       console.log(
+//         `connect to databases & listening on http://localhost:${PORT}/`
 //       );
-//     } catch (error) {
-//       console.log("Cannot connect to the server ...");
-//     }
+//     });
 //   })
 //   .catch((err) => {
-//     console.log("err =>", err);
+//     console.log(err);
 //   });
 
-app.listen(port, () =>
-  console.log(`The server running on: http://localhost:${port}`)
-);
+connect()
+  .then(() => {
+    try {
+      app.listen(PORT, () =>
+        console.log(`The server running on: http://localhost:${PORT}`)
+      );
+    } catch (error) {
+      console.log("Cannot connect to the server ...");
+    }
+  })
+  .catch((err) => {
+    console.log("err =>", err);
+  });
+
+// app.listen(port, () =>
+//   console.log(`The server running on: http://localhost:${port}`)
+// );
