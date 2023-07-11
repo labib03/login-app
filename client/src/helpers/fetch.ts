9bbb2d8a5d04bda9d38348724fee8afa5a8e9d2f
labib dev <ansorudinlabib1@gmail.com>
2023-07-11 23:55:11 +0700

    fix: fix some types issues and add authenticate func

---
+++ client/src/helpers/fetch.ts
@@ -0,0 +1,170 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+import { ResponseFetchType } from "../types/fetching.ts";
+import { BASE_URL } from "../datas/variables.ts";
+
+type ResponseProps = {
+  data: ResponseFetchType;
+  status?: number;
+};
+
+type UpdateUserProps = {
+  firstName?: string;
+  lastName?: string;
+  phoneNumber?: string;
+  username?: string;
+  email?: string;
+};
+
+const api: AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+});
+
+/** authenticate function */
+export async function authenticate(username: string) {
+  try {
+    return await axios.post("/api/authenticate", { username });
+  } catch (error) {
+    return { error: "Username doesn't exist...!" };
+  }
+}
+
+/** get User details */
+export async function getUser({ username }: { username: string }) {
+  try {
+    const { data } = await api.get(`/api/user/${username}`);
+    return { data };
+  } catch (error) {
+    return { error: "Password doesn't Match...!" };
+  }
+}
+
+/** register user function */
+export async function registerUser(credentials: {
+  username: string;
+  email: string;
+}) {
+  try {
+    const { data, status }: ResponseProps = await api.post(
+      `/api/register`,
+      credentials,
+    );
+
+    let { username, email } = credentials;
+    const { message } = data;
+
+    /** send email */
+    if (status === 201) {
+      await api.post("/api/registerMail", {
+        username,
+        userEmail: email,
+        text: message,
+      });
+    }
+
+    return Promise.resolve(message || "Success");
+  } catch (error) {
+    return Promise.reject({ error });
+  }
+}
+
+/** login function */
+export async function verifyPassword({
+  username,
+  password,
+}: {
+  username: string;
+  password: string;
+}) {
+  try {
+    if (username) {
+      const { data } = await api.post("/api/login", { username, password });
+      return Promise.resolve({ data });
+    }
+  } catch (error) {
+    return Promise.reject({ error: "Password doesn't Match...!" });
+  }
+}
+
+/** update user profile function */
+export async function updateUser(response: UpdateUserProps) {
+  try {
+    const token = await localStorage.getItem("token");
+    const data: AxiosResponse<ResponseFetchType> = await api.put(
+      "/api/updateUser",
+      response,
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
+
+    return Promise.resolve({ data });
+  } catch (error) {
+    return Promise.reject({ error: "Couldn't Update Profile...!" });
+  }
+}
+
+/** generate OTP */
+export async function generateOTP(username: string) {
+  try {
+    const { data: dataOTP, status }: ResponseProps = await api.get(
+      "/api/generateOTP",
+      { params: { username } },
+    );
+
+    const { code } = dataOTP;
+
+    // send mail with the OTP
+    if (status === 201) {
+      let { data } = await getUser({ username });
+
+      const { email } = data;
+
+      let text = `Your Password Recovery OTP is ${code}. Verify and recover your password.`;
+      await api.post("/api/registerMail", {
+        username,
+        userEmail: email,
+        text,
+        subject: "Password Recovery OTP",
+      });
+    }
+    return Promise.resolve(code);
+  } catch (error) {
+    return Promise.reject({ error });
+  }
+}
+
+/** verify OTP */
+export async function verifyOTP({
+  username,
+  code,
+}: {
+  username: string;
+  code: string;
+}) {
+  try {
+    const { data, status }: ResponseProps = await api.get("/api/verifyOTP", {
+      params: { username, code },
+    });
+    return { data, status };
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+/** reset password */
+export async function resetPassword({
+  username,
+  password,
+}: {
+  username: string;
+  password: string;
+}) {
+  try {
+    const { data, status }: ResponseProps = await api.put(
+      "/api/resetPassword",
+      { username, password },
+    );
+    return Promise.resolve({ data, status });
+  } catch (error) {
+    return Promise.reject({ error });
+  }
+}
