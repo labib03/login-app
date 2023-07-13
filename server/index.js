e38a8c36439c9a9e224e440b1babfa4bb03cbd3e
labib dev <ansorudinlabib1@gmail.com>
2023-07-13 14:29:17 +0700

    fix: fix PayloadTooLargeError on express server

--- server/index.js
+++ server/index.js
@@ -3,14 +3,19 @@ import express from "express";
 import morgan from "morgan";
 import connect from "./databases/connection.js";
 import { router } from "./router/route.js";
+import bodyParser from "body-parser";
 
 const app = express();
+const maxRequestBodySize = "50mb";
 
 /** middlewares */
+app.use(bodyParser.json({ limit: maxRequestBodySize }));
+app.use(bodyParser.urlencoded({ extended: true, limit: maxRequestBodySize }));
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
-app.disable("x-powered-by"); // fewer hackers know about our stack
+app.disable("x-powered-by");
+// fewer hackers know about our stack
 
 const PORT = 8080;
 // HTTPS Request
@@ -30,7 +35,7 @@ connect()
       // connectToMongo();
 
       app.listen(PORT, () =>
-        console.log(`The server running on: http://localhost:${PORT}`)
+        console.log(`The server running on: http://localhost:${PORT}`),
       );
     } catch (error) {
       console.log("Cannot connect to the server ...");
